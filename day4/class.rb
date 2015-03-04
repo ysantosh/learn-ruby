2e67685ecdefe30466218fd35291e548f7262597
Santosh Yadav <santoshy@sufferpeel-lm.eglbp.corp.yahoo.com>
2015-03-04 15:09:36 +0530

    test

--- day4/class.rb
+++ day4/class.rb
@@ -1,9 +1,28 @@
 class Yahoo
+    #attr_accessor will do the job of name= and name method
+    attr_accessor "company"
+    def hello
+        puts "hello world in class yahoo"
+    end
+    def name=(var)
+        @var = var
+    end
+    def name
+        puts @var
+    end
 end
 
 x=Yahoo.new
 puts x.class
 
 #there are some default methods which can be get through 
-puts x.methods
+#puts x.methods
 
+puts x.hello
+
+x.name="hello dear"
+x.name
+
+x.company="Yahoo"
+
+puts x.company
