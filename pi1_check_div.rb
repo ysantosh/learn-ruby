36a1c7cfa0803b99cabc0d8042115114ee04a78c
Santosh Yadav <santoshmcayadav@yahoo.co.in>
2016-12-14 01:55:13 +0530

    Update pi1_check_div.rb

--- pi1_check_div.rb
+++ pi1_check_div.rb
@@ -12,3 +12,40 @@
   end   
 end
 
+
+=begin
+output is
+4 - four
+6 - six
+8 - four
+12 - foursix
+16 - four
+18 - six
+20 - four
+24 - foursix
+28 - four
+30 - six
+32 - four
+36 - foursix
+40 - four
+42 - six
+44 - four
+48 - foursix
+52 - four
+54 - six
+56 - four
+60 - foursix
+64 - four
+66 - six
+68 - four
+72 - foursix
+76 - four
+78 - six
+80 - four
+84 - foursix
+88 - four
+90 - six
+92 - four
+96 - foursix
+100 - four
+=end
